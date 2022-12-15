16d7711135d02f84d87759487be1eb109c8dfe89
Чайковский Прохор <kemononya@yandex.ru>
2022-12-15 15:38:33 +0300

    add rollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import App from './components/App';
 import store from './slices/index';
 import './assets/application.css';
 import './i18next';
 
+const rollbarConfig = {
+  accessToken: 'fffd951913dc488e91be771f5fe1844e',
+  environment: 'production',
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-    <ToastContainer />
-  </Provider>,
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+        <ToastContainer />
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>,
 );
